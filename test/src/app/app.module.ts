b0077616ca3a8ce89901c03d25ae3d566a77bf5e
FabioAranzulla <fabio.aranzulla88@gmail.com>
2023-03-26 00:33:56 +0100

    first commit

---
+++ test/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+
+@NgModule({
+  declarations: [AppComponent, BookListComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
